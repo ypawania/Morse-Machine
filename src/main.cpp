fc6ddeeae35edcf5c3d30dc77624314a7e4974fa
Yohance <ypawania@gmail.com>
2025-05-29 18:45:34 -0400

    detect dots and dashes

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,18 @@ int myFunction(int, int);
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600);
+  Serial.println("H(ello, World!");
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  digitalWrite(LED_BUILTIN, HIGH); // turn the LED on
+  delay(1000);                     // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);  // turn the LED off
+  delay(1000);                     // wait for a second
+  Serial.println("LEDd Toggled!");
 }
 
 // put function definitions here:
